37b4bec9c508be5b31c8e4b81de0beb248ef4ab6
SH356398 <shipra.joshi2@wipro.com>
2020-03-03 00:40:00 +0530

    react setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                I am happyy :)  
+            </div>
+        )
+    }
+}
+
+export default App;
+
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require("webpack");
+var HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.html$/,
+          use:{
+            loader:"html-loader"
+          }
+        }
+      ]
+    },
+    plugins:[
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename:"index.html"
+      })
+    ]
+  };
